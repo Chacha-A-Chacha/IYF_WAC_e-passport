b29a0febe3fa02152fa0f3570a61d7576df149ae
Chacha-A-Chacha <calex2607@gmail.com>
2023-10-25 00:27:31 +0300

    admin routes for registration

---
+++ app/admin/forms.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class TeacherRegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    class_name = StringField('Class Name', validators=[DataRequired()])
+    course_name = StringField('Course Name', validators=[DataRequired()])
+    submit = SubmitField('Register Teacher')
+
+class StudentRegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    class_name = StringField('Class Name', validators=[DataRequired()])
+    additional_info = StringField('Additional Information')  # Add more fields if needed
+    submit = SubmitField('Register Student')
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,20 +1,10 @@
 #!/src/bin/env python3
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
 import pandas as pd
-from app import db  # Import db from the main __init__.py file
-from ..models import User, Student
+from .forms import TeacherRegistrationForm, StudentRegistrationForm
+from ..models import db, Teacher, Student, Class, Course
 from . import admin_bp
-from .utils import generate_temp_password
-
-
-# Student model (example)
-# class Student(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     student_id = db.Column(db.String(20), unique=True, nullable=False)
-#     full_name = db.Column(db.String(120), nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     course = db.Column(db.String(80), nullable=False)
-#     password = db.Column(db.String(120), nullable=False)  # Hashed password stored in the database
+from .utils import generate_temp_password, send_temp_password_email
 
 
 @admin_bp.route('/admin_dashboard')
@@ -35,78 +25,141 @@ def admin_dashboard():
 # Route to submit the Excel sheet and register students
 @admin_bp.route('/submit-excel', methods=['POST'])
 def submit_excel():
+    """
+    Handle the submission of an Excel file containing student data, process the data, and register students.
+    Display flash messages for success or error.
+
+    Returns:
+        str: A redirect response to the admin dashboard page after processing the Excel file.
+    """
     if 'file' not in request.files:
-        return "No file part"
+        flash('No file part', 'error')
+        return redirect(request.url)
 
     file = request.files['file']
     if file.filename == '':
-        return "No selected file"
+        flash('No selected file', 'error')
+        return redirect(request.url)
 
-    if file:
-        # Read data from Excel
-        df = pd.read_excel(file)
-        # Process and register students
-        for index, row in df.iterrows():
-            full_name = row['Full Name']
-            email = row['Email']
-            course = row['Course']
+    try:
+        if file:
+            # Read data from Excel
+            df = pd.read_excel(file)
+            # Process and register students
+            for index, row in df.iterrows():
+                full_name = row['Full Name']
+                email = row['Email']
+                course = row['Course']
 
-            # Generate student ID, temporary password, and register student
-            # student_id = generate_student_id(full_name)
-            temp_password = generate_temp_password()
-            student = Student(full_name=full_name, email=email, course=course,
-                              password=temp_password)
-            db.session.add(student)
-            db.session.commit()
-        return "Students registered successfully!"
+                # Generate student ID, temporary password, and register student
+                temp_password = generate_temp_password()
+                student = Student(full_name=full_name, email=email, course=course, password=temp_password)
+                db.session.add(student)
+                db.session.commit()
 
-    return "Error processing file"
+                # Send temporary password email
+                send_temp_password_email(email, student.student_id, temp_password)
+
+            flash('Students registered successfully!', 'success')
+            return redirect(url_for('admin_bp.admin_dashboard'))
+    except Exception as e:
+        print(str(e))  # Log the exception for debugging
+        db.session.rollback()  # Rollback changes if there's an error
+        flash('Error processing file. Please try again.', 'error')
+
+    return redirect(request.url)
 
 
 @admin_bp.route('/admin/register-student', methods=['GET', 'POST'])
 def admin_register_student():
+    """
+    Handle the registration of a new student by the admin.
+    Display a registration form for the admin to input student details.
+    Register the student in the database and display flash messages for success or error.
+
+    Returns:
+        str: A redirect response to the admin dashboard or the registration form page based on form submission.
+    """
     form = StudentRegistrationForm()
     if form.validate_on_submit():
+
         try:
-            full_name = form.full_name.data
+            username = form.username.data
             email = form.email.data
-            password = generate_temp_password()  # Generate temporary password
+            password = generate_temp_password()
+            class_name = form.class_name.data
+            # additional_info = form.additional_info.data
 
             # Register student
-            student = Student(full_name=full_name, email=email, password=password)
+            student = Student(username=username, email=email, password=password, role='student')
+            # Assuming Student model has additional_info field
+            # student.additional_info = additional_info
             db.session.add(student)
             db.session.commit()
 
-            return "Student registered successfully!"
+            # Handle class relationship
+            # Assuming Class model has appropriate relationships defined
+            class_obj = Class.query.filter_by(class_name=class_name).first()
+            student.class_enrollment = class_obj
+            db.session.commit()
+
+            # Send temporary password email
+            send_temp_password_email(email, student.student_id, password)
+
+            flash('Student registered successfully!', 'success')
+            return redirect(url_for('admin_bp.admin_dashboard'))
         except Exception as e:
             print(str(e))  # Log the exception for debugging
             db.session.rollback()  # Rollback changes if there's an error
-            return "Error registering student"
+            flash('Error registering student. Please try again.', 'error')
 
     return render_template('admin/register_student.html', form=form)
 
+
 @admin_bp.route('/admin/register-teacher', methods=['GET', 'POST'])
 def admin_register_teacher():
+    """
+    Handle the registration of a new teacher by the admin.
+    Display a registration form for the admin to input teacher details.
+    Register the teacher in the database and display flash messages for success or error.
+
+    Returns:
+        str: A redirect response to the admin dashboard or the registration form page based on form submission.
+    """
     form = TeacherRegistrationForm()
     if form.validate_on_submit():
         try:
-            full_name = form.full_name.data
+            username = form.username.data
             email = form.email.data
             password = generate_temp_password()  # Generate temporary password
-            subject = form.subject.data
+            class_name = form.class_name.data
+            course_name = form.course_name.data
 
             # Register teacher
-            teacher = Teacher(full_name=full_name, email=email, password=password, subject=subject)
+            teacher = Teacher(username=username, email=email, password=password, role='teacher')
             db.session.add(teacher)
             db.session.commit()
 
-            return "Teacher registered successfully!"
+            # Handle class and course relationships
+            # Assuming Class and Course models have appropriate relationships defined
+            class_obj = Class.query.filter_by(class_name=class_name).first()
+            course_obj = Course.query.filter_by(course_name=course_name).first()
+            teacher.class_teacher = class_obj
+            teacher.course_teacher = course_obj
+            db.session.commit()
+
+            # Send temporary password email
+            send_temp_password_email(email, teacher.teacher_id, password)
+
+            flash('Teacher registered successfully!', 'success')
+            return redirect(url_for('admin_bp.admin_dashboard'))
         except Exception as e:
             print(str(e))  # Log the exception for debugging
             db.session.rollback()  # Rollback changes if there's an error
-            return "Error registering teacher"
+            flash('Error registering teacher. Please try again.', 'error')
 
     return render_template('admin/register_teacher.html', form=form)
+
 
 # Helper function to generate a student ID
 # def generate_student_id(full_name):
@@ -123,11 +176,11 @@ def admin_register_teacher():
 
 
 # Helper function to send registration email
-def send_registration_email(email, student_id, temp_password):
-    msg = Message('Your Student ID and Temporary Password',
-                  sender='your-email@example.com', recipients=[email])
-    msg.body = f'Your Student ID: {student_id}\nTemporary Password: {temp_password}\nPlease reset your password after login.'
-    mail.send(msg)
+# def send_registration_email(email, student_id, temp_password):
+#     msg = Message('Your Student ID and Temporary Password',
+#                   sender='your-email@example.com', recipients=[email])
+#     msg.body = f'Your Student ID: {student_id}\nTemporary Password: {temp_password}\nPlease reset your password after login.'
+#     mail.send(msg)
 
 
 # Admin route to display registered students
