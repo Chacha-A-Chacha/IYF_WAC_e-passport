a14f9aeae18efa402ade7655af4d92d9d1c72de4
Chacha-A-Chacha <calex2607@gmail.com>
2023-10-24 20:21:54 +0300

    def create_app to accept the env="development" arg

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from app import create_app, db, models
-# from flask_script import Manager
-from flask_migrate import Migrate  # upgrade
+import os
+from app import create_app, db
+from flask_migrate import Migrate
 
-# Create the Flask app using the create_app function from the app package
-app = create_app()
+# Get the environment from the FLASK_ENV variable; default to 'development' if not set
+env = os.environ.get('FLASK_ENV', 'development').lower()
+
+# Create the Flask app using the appropriate configuration class based on the environment
+app = create_app(env)
+
+# Initialize Flask-Migrate with the Flask app and the SQLAlchemy database instance
 migrate = Migrate(app, db)
-# manager = Manager(app)
-
-
-# Use the app context to work within the application
-with app.app_context():
-    # Now you can access parts of your application that require the app context, such as database operations
-    # For example, you can create tables if using SQLAlchemy
-    db.create_all()
 
 if __name__ == '__main__':
     # Run the Flask app
     app.run()
+
+"""
+from app import create_app, db  # Import necessary modules from the app package
+from flask_migrate import Migrate
+
+# Create the Flask app using the create_app function from the app package
+app = create_app()
+migrate = Migrate(app, db)
+
+# Use the app context to work within the application
+with app.app_context():
+    # Apply database migrations
+    migrate.init_app(app, db)
+    
+    # Now you can access parts of your application that require the app context, such as database operations
+    # For example, you can create tables if using SQLAlchemy (commented out in this snippet as migrations handle this)
+    # db.create_all()  # Avoid creating tables here; use migrations to manage schema changes
+
+if __name__ == '__main__':
+    # Run the Flask app
+    app.run()
+
+"""
--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail, Message
+from flask_mail import Mail
 
 from config import config
 
@@ -13,16 +13,36 @@ login_manager = LoginManager()
 bcrypt = Bcrypt()
 mail = Mail()
 
+def create_app(env='development'):
+    """
+    Creates and configures the Flask application.
 
-def create_app():
+    Parameters:
+        env (str): The environment for which the application is configured. Default is 'development'.
+
+    Returns:
+        Flask: The configured Flask application instance.
+
+    The create_app function initializes and configures the Flask application by loading the appropriate
+    configuration based on the specified environment. It sets up extensions such as SQLAlchemy, Flask-Migrate,
+    Flask-Login, Flask-Bcrypt, and Flask-Mail. Blueprints for different parts of the application (main, authentication,
+    admin, teacher) are registered here, allowing for modular and organized project structure.
+
+    Example Usage:
+        To create an application for the development environment:
+        >>> app = create_app(env='development')
+
+        To create an application for the production environment:
+        >>> app = create_app(env='production')
+    """
     app = Flask(__name__)
 
-    app.config.from_object(config["development"])
-    config["development"].init_app(app)
+    # Select appropriate configuration based on the environment
+    app.config.from_object(config.get(env, config['development']))
+    config[env].init_app(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
-
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'  # Specify the login view for authentication
 
