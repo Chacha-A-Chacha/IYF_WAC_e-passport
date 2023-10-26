0ce9d46d559ac5aa3fa93832902be9d41abbe67e
Chacha-A-Chacha <calex2607@gmail.com>
2023-10-27 02:36:54 +0300

    modified:   app/main/routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 #!/src/bin/env python3
 from flask import Flask, request, render_template, flash, redirect, url_for
-from .forms import 
+from .forms import ProfileForm
 from ..models import db, Teacher, Student, Class, Course
-from . import admin_bp
+from . import main
 from .utils import generate_temp_password, send_temp_password_email
